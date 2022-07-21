440b1ae6c97cef7583d69d37e71563771e81e9eb
yaroslavasavina <savina.yaroslava.igorevna@gmail.com>
2022-07-21 11:11:52 +0300

    task_1_1

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,16 @@ public class Main {
         array2[2] = 9.986;
         System.out.println(Arrays.toString(array2));
 
+        // Task 1_3
+
+        int [] boxes = {2,4,7};
+        int applesBox = boxes[0];
+        int orangesBox = boxes[1];
+        int lemonBox = boxes[2];
+        System.out.println(applesBox);
+        System.out.println(orangesBox);
+        System.out.println(lemonBox);
+
+
     }
 }
