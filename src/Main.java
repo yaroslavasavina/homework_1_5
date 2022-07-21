11ac46264d8139a6ad58d064a3f893ad4a154566
yaroslavasavina <savina.yaroslava.igorevna@gmail.com>
2022-07-21 10:48:08 +0300

    task_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+     // Task 1_1
+        int [] array = new int [3];
+         array[0] = 1;
+         array[1] = 2;
+         array[2] = 3;
+        System.out.println(Arrays.toString(array));
 
+        // Task 1_2
+        double[] array2 = {1.57, 7.654, 9.986};
+        array2[0] = 1.57;
+        array2[1] = 7.654;
+        array2[2] = 9.986;
+        System.out.println(Arrays.toString(array2));
 
     }
 }
