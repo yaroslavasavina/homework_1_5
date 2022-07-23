b54d811ea6d65f159b49f3019134b70c0372d65b
yaroslavasavina <savina.yaroslava.igorevna@gmail.com>
2022-07-23 10:16:26 +0300

    task_correct

--- src/Main.java
+++ src/Main.java
@@ -8,33 +8,63 @@ public class Main {
         array[0] = 1;
         array[1] = 2;
         array[2] = 3;
-            System.out.println(Arrays.toString(array));
-  //      for (int i = array.length - 1; i > 0; i--)
-  //          System.out.print(array[i] + ",");
-  //          System.out.println(array[0]);
+        for (int i = 0; i < array.length ; i++) {
+            if (i < array.length-1) {
+                System.out.print(array[i] + ", ");
+            } else {
+                System.out.println(array[2]);
+            }
+        }
+        for (int i = array.length-1; i >=0 ; i--) {
+            if(i<= array.length-1 && i>0 ){
+                System.out.print(array[i]+", ");
+            }else {
+                System.out.print(array[i]);
+            }
+        }
+                System.out.println();
 
           for (int i = 0; i < array.length; i++) {
             if (i % 2 != 0) {
-            System.out.println(i + 1);
-          } else {
-            System.out.println(i);
+                System.out.println(i + 1);
+          } else
+                System.out.println(i);{
           }
        }
-            System.out.println();
+                System.out.println();
 
         double[] arr = {1.57, 7.654, 9.986};
-            System.out.println(Arrays.toString(arr));
-        for (int y = arr.length-1; y>0 ; y--)
-            System.out.print(arr[y]+", ");
-            System.out.print(arr[0]);
+        for (int a = 0; a < arr.length; a++) {
+            if (a<arr.length-1) {
+                System.out.print(arr[a] + ", ");
+            }else{
+                System.out.print(arr[a]);
+            }
+        }
+                System.out.println();
 
-            System.out.println();
+        for (int a = arr.length-1; a>0 ; a--) {
+                System.out.print(arr[a] + ", ");
+                System.out.print(arr[0]);
+        }
+                System.out.println();
 
         int [] boxes = {5,6,7,8,9};
-            System.out.println(Arrays.toString(boxes));
-        for (int a = boxes.length-1; a>0 ; a--)
-            System.out.print(boxes[a]+", ");
-            System.out.print(boxes[0]);
+        for (int y = 0; y < boxes.length ; y++) {
+            if (y < boxes.length-1){
+                System.out.print(boxes[y] + ", ");
+            }else{
+                System.out.print(boxes[y]);
+            }
+        }
+                System.out.println();
 
+        for (int y = boxes.length-1; y>=0 ; y--) {
+            if (y <= boxes.length-1 && y > 0) {
+                System.out.print(boxes[y] + ", ");
+            } else {
+                System.out.print(boxes[0]);
+            }
+        }
     }
 }
